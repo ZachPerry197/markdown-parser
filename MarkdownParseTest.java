e9410d86c09832758d5546714facb3850cfa5362
ZachPerry197 <102757693+ZachPerry197@users.noreply.github.com>
2022-05-22 00:22:14 -0700

    added lab report 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,81 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-public class MarkdownParseTest {
+import java.util.*;
+
+public class MarkdownParseTest extends MarkdownParse {
 
     @Test
     public void additionTest() {
-        assertEquals(2, 1 + 2);
+        assertEquals(3, 1 + 2);
     }
 
+    @Test
+    public void snippet1Test() {
+        ArrayList<String> correctList = new ArrayList<>();
+        correctList.add("`google.com");
+        correctList.add("google.com");
+        correctList.add("ucsd.edu");
+        String snippet1 = """
+            `[a link`](url.com)
+
+            [another link](`google.com)`
+            
+            [`cod[e`](google.com)
+            
+            [`code]`](ucsd.edu)
+                """;
+        ArrayList<String> outputList = getLinks(snippet1);
+        assertEquals(correctList, outputList);
+    }
+
+    @Test
+    public void snippet2Test() {
+        ArrayList<String> correctList = new ArrayList<>();
+        correctList.add("a.com");
+        correctList.add("a.com(())");
+        correctList.add("example.com");
+        String snippet2 = """
+            [a [nested link](a.com)](b.com)
+
+            [a nested parenthesized url](a.com(()))
+            
+            [some escaped \\[ brackets \\]](example.com)
+                """;
+        ArrayList<String> outputList = getLinks(snippet2);
+        assertEquals(correctList, outputList);
+    }
+
+    @Test
+    public void snippet3Test() {
+        ArrayList<String> correctList = new ArrayList<>();
+        correctList.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+        String snippet3 = """
+            [this title text is really long and takes up more than 
+            one line
+            
+            and has some line breaks](
+                https://www.twitter.com
+            )
+            
+            [this title text is really long and takes up more than 
+            one line](
+            https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule
+            )
+            
+            
+            [this link doesn't have a closing parenthesis](github.com
+            
+            And there's still some more text after that.
+            
+            [this link doesn't have a closing parenthesis for a while](https://cse.ucsd.edu/
+            
+            
+            
+            )
+            
+            And then there's more text
+                """;
+        ArrayList<String> outputList = getLinks(snippet3);
+        assertEquals(correctList, outputList);
+    }
 }
